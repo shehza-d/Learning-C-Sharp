0ed7aa5e9b6f8188055a4c2cd0992e41eb1ad852
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-24 00:46:49 +0500

    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ

---
+++ C# Hello world/program.cs
@@ -0,0 +1,32 @@
+// C# program to print Assalam-O-Alaikum World!
+using System;
+  
+// namespace declaration
+namespace HelloWorldApp {
+      
+    // Class declaration
+    class Shehzad {
+          
+        // Main Method
+        static void Main(string[] args) {
+              
+            // statement
+            Console.WriteLine("Assalam-O-Alaikum World!");
+              
+            // To prevents the screen from 
+            // running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
+
+
+// namespace HelloWorld
+// {
+//     class Hello {         
+//         static void Main(string[] args)
+//         {
+//             System.Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
